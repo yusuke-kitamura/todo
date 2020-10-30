fd4c69a24c31b98cc7c94e8c19b86426b07b9504
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-10-30 17:11:19 +0900

    to get tasks from tasktable

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,12 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Folder;
 use Illuminate\Http\Request;
+use App\Task;
 
 class TaskController extends Controller
 {
 	public function index(int $id){
+		// すべてのフォルダーを取得
 		$folders = Folder::all();
-		return view('tasks.index',['folders' => $folders, 'current_folder_id' => $id]);
+		//フォルダーを取得
+		$current_folder = Folder::find($id);
+		// 選ばれたフォルダーに紐づくタスクを取得
+		$tasks = Task::where('folder_id', $current_folder->id)->get();
+		return view('tasks.index',[
+			'folders' => $folders,
+			'current_folder_id' => $current_folder->id,
+			'tasks'=>$tasks]);
 	}
 
 }
