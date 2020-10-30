846784e7efea42aa2286e2eee52fe689743d8fd0
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-10-30 16:01:06 +0900

    add controllers and models

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Folder;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+	public function index(){
+		$folders = Folder::all();
+		return view('tasks.index',['folders' => $folders]);
+	}
+
+}
