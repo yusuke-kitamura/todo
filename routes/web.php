d33459d17eebb470b15c9df032921dcc4fe4b007
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-11-01 08:31:08 +0900

    update route due to error

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@
 
 // タスク一覧画面を表示
 Route::get('/folders/{id}/tasks','TaskController@index')->name('tasks.index');
+//タスクの新規作成画面と登録
+Route::get('/folders/{id}/tasks/create','TaskController@create')->name('tasks.create');
+Route::post('/folders/{id}/tasks/create','TaskController@store');
 // フォルダの新規作成画面と登録
 Route::get('/folders/create','FolderController@create')->name('folders.create');
 Route::post('/folders/create','FolderController@store');
